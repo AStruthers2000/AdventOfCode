ffd81fe139caece2668e827e13d87d6ac61a1cb9
Andrew Struthers <struthers919@gmail.com>
2023-11-28 11:18:57 -0800

    First commit

---
+++ C++/AdventOfCode/2022/Day3/Year2022_Day3.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "../../Problem.h"
+
+class Year2022_Day3 : public Problem
+{
+public:
+    Year2022_Day3() : Problem("2022 Day 3")
+    {
+        filepath = R"(./2022/Day3/)";
+    }
+    
+    float Problem1() override;
+    float Problem2() override;
+
+private:
+    vector<string> SplitRucksackInTwo(const string& rucksack) const;
+    char FindMatchingRucksackItem(vector<string> rucksack) const;
+    int GetItemOrder(char item) const;
+};
