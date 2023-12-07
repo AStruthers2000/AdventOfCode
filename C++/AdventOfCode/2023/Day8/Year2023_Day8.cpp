40ff3e1f6706d7e1c40496d0c8d71d17fa0785ea
AStruthers2000 <struthers919@gmail.com>
2023-12-07 00:03:04 -0800

    Created day 8 template

---
+++ C++/AdventOfCode/2023/Day8/Year2023_Day8.cpp
@@ -0,0 +1,11 @@
+﻿#include "Year2023_Day8.h"
+
+string Year2023_Day8::Problem1()
+{
+    return "";
+}
+
+string Year2023_Day8::Problem2()
+{
+    return "";
+}
---
+++ C++/AdventOfCode/2023/Day8/Year2023_Day8.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "../../Problem.h"
+
+class Year2023_Day8 : public Problem
+{
+public:
+    Year2023_Day8() : Problem("Year 2023 Day 8: ")
+    {
+        filepath = R"(./2023/Day8/)";
+    }
+
+    string Problem1() override;
+    string Problem2() override;
+
+private:
+    
+};
